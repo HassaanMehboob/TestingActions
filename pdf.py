9ec68060f6e1dd4d2f06193c929371aded627baf
HassaanMehboob <97082404+HassaanMehboob@users.noreply.github.com>
2022-01-19 10:27:32 +0500

    Add files via upload

---
+++ pdf.py
@@ -0,0 +1,13 @@
+# import pandas to create dataframe
+import pandas as pd
+# import pdfkit to convert the file into pdf
+import pdfkit
+
+def converting_to_pdf():
+    df = pd.read_csv('output.csv', index_col=0)
+    df.head()
+    f = open('output.html','w')
+    a = df.to_html()
+    f.write(a)
+    f.close()
+    pdfkit.from_file('output.html', 'output.pdf')
